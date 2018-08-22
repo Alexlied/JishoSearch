9de85adf2b602cf3874fe86a69003a5db5646a94
alexlied <alexlied@gmail.com>
2018-08-22 13:47:00 -0700

    context menu includes selected text

--- background.js
+++ background.js
@@ -6,4 +6,4 @@ function jishosearch(info)
  chrome.tabs.create({url: "https://jisho.org/search/" + searchstring})
 }
 
-chrome.contextMenus.create({title: "Search Jisho.org", contexts:["selection"], onclick: jishosearch});
+chrome.contextMenus.create({title: 'Search Jisho.org for "%s"', contexts:["selection"], onclick: jishosearch});
