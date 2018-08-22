3266223611459c16cd43a4f4b38b3337770e7599
alexlied <alexlied@gmail.com>
2018-08-22 15:35:02 -0700

    added larger icon

--- background.js
+++ background.js
@@ -12,5 +12,19 @@ function jishoSearchKanji(info)
  chrome.tabs.create({url: "https://jisho.org/search/" + searchstring + "%20%23kanji"})
 }
 
-chrome.contextMenus.create({title: 'Search Jisho.org for "%s"', contexts:["selection"], onclick: jishoSearchWord});
-//chrome.contextMenus.create({title: 'Search Jisho.org for "%s" as kanji', contexts:["selection"], onclick: jishoSearchKanji});
+function jishoSearchSentences(info)
+{
+ var searchstring = info.selectionText;
+ chrome.tabs.create({url: "https://jisho.org/search/" + searchstring + "%20%23sentences"})
+}
+
+function jishoSearchNames(info)
+{
+ var searchstring = info.selectionText;
+ chrome.tabs.create({url: "https://jisho.org/search/" + searchstring + "%20%23names"})
+}
+
+chrome.contextMenus.create({title: 'Search for "%s"', contexts:["selection"], onclick: jishoSearchWord});
+chrome.contextMenus.create({title: 'Search kanji for "%s"', contexts:["selection"], onclick: jishoSearchKanji});
+chrome.contextMenus.create({title: 'Search sentences for "%s"', contexts:["selection"], onclick: jishoSearchSentences});
+chrome.contextMenus.create({title: 'Search names for "%s"', contexts:["selection"], onclick: jishoSearchNames});
